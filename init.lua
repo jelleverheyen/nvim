8362aac5fe982a1f3aa29454cd41820209f8bd8d
Jelle <jelle.verheyen@pm.me>
2024-10-22 20:05:42 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.g.mapleader = ' '
+vim.g.localmapleader = ' '
+
+vim.g.have_nerd_font = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.signcolumn = 'yes'
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+vim.opt.showmode = true
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+})
---
+++ lua/plugins/vim-sleuth.lua
@@ -0,0 +1,4 @@
+-- Automatically detect shiftwidth and tabstop
+return {
+  'tpope/vim-sleuth'
+}
