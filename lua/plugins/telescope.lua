3fe99558f631229d4c3e85483ebab7803c173125
Jelle <jelle.verheyen@pm.me>
2024-10-25 17:22:25 +0200

    feat(telescope): Add live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,6 +37,7 @@ return {
 
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Grep files" })
+    vim.keymap.set("n", "<leader>sf", builtin.live_grep, { desc = "Search text in project" })
   end,
 }
